1d49d90dffaba585102f16a86b276f508a645dda
Samuel Obeng <61053627+samlobeng@users.noreply.github.com>
2022-02-27 15:53:01 +0100

    added routes for getting, updating and deleting seeding jobs

--- Back-end/src/services/seedingJob.js
+++ Back-end/src/services/seedingJob.js
@@ -20,4 +20,58 @@ seedingRouter.post("/", async (req, res, next) => {
     }
 });
 
+seedingRouter.get('/', async (req, res, next) => {
+    try {
+        const response = await SeedingModel.find();
+        res.status(201).send(response);
+    } catch (error) {
+        console.log(error);
+        next(error);
+    }
+});
+
+seedingRouter.get('/:id', async(req, res, next) => {
+    try {
+        const seeding = await SeedingModel.findById(req.params.id);
+        res.status(200).json(seeding);
+
+    } catch (error) {
+        res.status(500).json(error)
+    }
+})
+
+seedingRouter.put('/:id', async (req, res, next) => {
+        try {
+            const updatedSeeding = await SeedingModel.findByIdAndUpdate(
+                req.params.id,
+                {
+                    $set: req.body,
+                },
+                {
+                    new: true
+                }
+            )
+            res.status(200).json(updatedSeeding);
+
+        } catch (error) {
+            res.status(500).json(error);
+        }
+});
+
+seedingRouter.delete('/:id', async (req, res, next) => {
+    try {
+        const deletedSeeding = await SeedingModel.findByIdAndDelete(req.params.id);
+        if (deletedSeeding) {
+            res.status(204).send();
+        } else {
+            next(createError(404, `Seeding with _id ${req.params.id} not found!`));
+        }
+        res.status(204).send();
+    } catch (error) {
+        next(error);
+    }
+});
+
+
+
 export default seedingRouter;
