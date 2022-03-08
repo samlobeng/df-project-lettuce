cf56eb73a4c243cd370ccb048bdb05c974708ca9
miavaghi@rhrk.uni-kl.de <mn.mohtasham@gmail.com>
2022-03-08 14:10:14 +0100

    delete function on seeding jobs

--- Front-end/app.js
+++ Front-end/app.js
@@ -1,12 +1,44 @@
 import 'regenerator-runtime/runtime'
 import {plantingSeeds} from "./plantingExecution.js";
-
 const btnExecute = document.querySelector(".button")
 const btnRUN = document.getElementById("button")
+const btnUpdate = document.querySelector(".updateButton")
+const btnDelete = document.querySelector(".deleteButton")
 const endpoint = "http://localhost:3001/seedingjob"
 
+
+
+// load records on load
+window.onload = function(){
+    loadTable()
+}
+
+function loadTable(){
+    fetch(endpoint)
+        .then((response)=>{return response.json();})
+        .then((myJson)=>{
+            draw(myJson)
+        })
+}
+
 // making the table of existing seedingjobs
+// function draw(t){
+//     const table = document.getElementById('myTable')
+//     for (let i=0;i<t.length;i++){
+//         const row = `<tr>
+//                 <td><input type="radio" class="class_radio" name="checker""></input></td>
+//                 <td>${t[i]._id}</td>
+//                 <td>${t[i].top_left}</td>
+//                 <td>${t[i].bottom_right}</td>
+//                 <td>${t[i].density}</td>
+//                 <td>${t[i].depth}</td>
+//                 <td>${t[i].createdAt}</td>
+//                 </tr>`
+//         table.innerHTML+= row
+//     }
+// }
 function draw(t){
+    $("#myTable").empty();
     const table = document.getElementById('myTable')
     for (let i=0;i<t.length;i++){
         const row = `<tr>
@@ -22,65 +54,55 @@ function draw(t){
     }
 }
 
+// function updateTable() {
+//     $("#myTable tr").remove();
+//     $.ajax({
+//         type: "GET",
+//         url: endpoint,
+//         dataType: "json",
+//         async: true,
+//         success: function () {
+//             loadTable()
+//         }
+//     });
+// }
+
 function updateTable() {
-    $("#myTable tr").remove();
-    $.ajax({
-        type: "GET",
-        url: endpoint,
-        dataType: "json",
-        async: true,
-        success: function () {
-            loadTable()
-        }
-    });
+    loadTable()
 }
-
-
-function loadTable(){
-    fetch(endpoint)
-        .then((response)=>{return response.json();})
-        .then((myJson)=>{
-            draw(myJson)
-        })
-}
-
 
 function select_row() {
     const radios = document.getElementsByName("checker")
-    for (let i = 0;i<radios.length;i++){
-        if (radios[i].checked == true){
-            // const row = radios[i].parentNode.parentNode.textContent
+    for (let i = 0; i < radios.length; i++) {
+        if (radios[i].checked == true) {
+                // const row = radios[i].parentNode.parentNode.textContent
             const selected_job_ID = radios[i].parentNode.parentNode.children[1].textContent
-            return  selected_job_ID
+            return selected_job_ID
             //alert('selected_job_ID:' + selected_job_ID);
         }
     }
 }
 
-async function fetchID(){
-    const id =  select_row()
+async function fetchID() {
+    const id = select_row()
     try {
-        const response = await fetch(endpoint+"/"+id)
-        if (response.ok){
+        const response = await fetch(endpoint + "/" + id)
+        if (response.ok) {
             const data = await response.json()
             console.log(data)
             return data
         }
-    }catch (error){}
+    } catch (error) {
+    }
 
-}
-
-// load records on load
-window.onload = function(){
-    loadTable()
 }
 
 
 //Runs when we press the save button
-const handleSubmit = async (event) =>{
+const handleSubmit = async (event) => {
     event.preventDefault()
 
-    //getting data from the form and make data onject
+        //getting data from the form and make data onject
     const myData = {
         top_left: [parseInt(document.getElementById("tl-x").value), parseInt(document.getElementById("tl-y").value)],
         bottom_right: [parseInt(document.getElementById("br-x").value), parseInt(document.getElementById("br-y").value)],
@@ -89,40 +111,61 @@ const handleSubmit = async (event) =>{
     }
 
     try {
-    const response = await fetch(endpoint,{
-        method: "POST",
-        body: JSON.stringify(myData),
-        headers:{
-            "Content-type": "application/json",
-        },
+        const response = await fetch(endpoint, {
+                method: "POST",
+                body: JSON.stringify(myData),
+                headers: {
+                    "Content-type": "application/json",
+                },
         })
-        if(response.ok) {
+        if (response.ok) {
             console.log(response.body);
             updateTable()
 
         }
 
-    }
-    catch  (error){
+    } catch (error) {
 
     }
 }
 
 ////////////////handleRUN to run seeding job
 const handleRUN = async (event) => {
+    const {top_left, bottom_right, density, depth} = await fetchID()
+    plantingSeeds(top_left[0], bottom_right[0], top_left[1], bottom_right[1], density, depth)
 
-    const {top_left, bottom_right, density,depth} = await fetchID()
-    plantingSeeds(top_left[0],bottom_right[0],top_left[1],bottom_right[1], density, depth)
-
-    //EDIT THIS to call for SEEDING FUNCTION
-
+        //EDIT THIS to call for SEEDING FUNCTION
 
 
+}
+
+////////////////handleUpdate to delete a seeding job
+const handleDelete = async () => {
+    const id =  select_row()
+    try {
+        const response = await fetch(endpoint+"/"+id, {
+            method: 'DELETE'
+        })
+        if (response.ok){
+            updateTable()
+            console.log("Seeding job removed!");
+        }
+
+    }catch (error){
+        throw error
+    }
+}
+
+////////////////handleUpdate to update a seeding job
+const handleUpdate = async () => {
+    console.log("updating the job")
 }
 
 
 btnExecute.addEventListener("click", handleSubmit)
 btnRUN.addEventListener("click", handleRUN)
+// btnUpdate.addEventListener('click', handleUpdate)
+btnDelete.addEventListener('click', handleDelete)
 
 
 
