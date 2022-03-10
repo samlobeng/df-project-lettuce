14692accf72901ce108aab3e70bd80adc00cd689
Rohit Kumar <rohkumar@rhrk.uni-kl.de>
2022-03-10 11:31:57 +0000

    updated with bot status changes

--- Back-end/src/services/botStatus.js
+++ Back-end/src/services/botStatus.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import SeedingModel from '../models/seedingSchema.js';
+import botStatusModel from '../models/statusSchema.js';
 import createError from "http-errors";
 
 const statusRouter = express.Router();
@@ -7,7 +7,7 @@ const statusRouter = express.Router();
 
 statusRouter.get('/', async (req, res, next) => {
     try {
-        const response = await SeedingModel.find();
+        const response = await botStatusModel.find();
         res.status(201).send(response);
     } catch (error) {
         console.log(error);
@@ -15,14 +15,14 @@ statusRouter.get('/', async (req, res, next) => {
     }
 });
 
-seedingRouter.get('/:id', async(req, res, next) => {
-    try {
-        const seeding = await SeedingModel.findById(req.params.id);
-        res.status(200).json(seeding);
+// seedingRouter.get('/:id', async(req, res, next) => {
+//     try {
+//         const seeding = await botStatusModel.findById(req.params.id);
+//         res.status(200).json(seeding);
 
-    } catch (error) {
-        res.status(500).json(error)
-    }
-})
+//     } catch (error) {
+//         res.status(500).json(error)
+//     }
+// })
 
 export default statusRouter;
